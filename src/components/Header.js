4675b4959277c3e7d66cf429814625824c1ef518
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-08-29 23:18:41 +0530

    Refactor code and styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,84 +1,98 @@
 import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
 import "./Header.css"
 import logo from '../logo192.png';
 import DeleteIcon from '@material-ui/icons/Delete';
-import { IconButton} from '@material-ui/core';
-import CircularProgress from '@material-ui/core/CircularProgress'
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import { IconButton } from '@material-ui/core';
+import Snackbar from '@material-ui/core/Snackbar';
+import HourglassEmptyIcon from '@material-ui/icons/HourglassEmpty';
+import DateRangeIcon from '@material-ui/icons/DateRange';
+import Divider from '@material-ui/core/Divider';
+import CircularProgressWithLabel from './CircularProgressWithLabel';
+import Card from './Card'
 
-function CircularProgressWithLabel(props) {
-  return (
-    <Box position="relative" display="inline-flex">
-      <CircularProgress variant="determinate" {...props} size="10rem"/>
-      <Box
-        top={0}
-        left={0}
-        bottom={0}
-        right={0}
-        position="absolute"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Typography variant="caption" component="div" color="textSecondary" variant="h3">{isNaN(props.value)?'':`${Math.round(
-          props.value,
-        )}%`}</Typography>
-      </Box>
-    </Box>
-  );
-}
 export default function Header(props) {
     const [item, setCurrentItem] = useState(null);
-    const [count,setCount] = useState(0);
-    const [checked,setChecked] = useState(false)
-    const [progress,setProgress] = useState(0);
+    const [count, setCount] = useState(0);
+    const [checked, setChecked] = useState(false)
+    const [progress, setProgress] = useState(0);
     const [items, ItemsList] = useState([])
     const [val, keyVal] = useState(0)
 
-    useEffect(()=>{calProgress()},[count,items.length])
+    const [state, setState] = React.useState({
+        open: false,
+        vertical: 'top',
+        horizontal: 'center',
+    });
+
+    const { vertical, horizontal, open } = state;
+
+    const handleClick = (newState) => {
+        setState({ open: true, ...newState });
+    };
+    const handleClose = () => {
+        setState({ ...state, open: false });
+    };
+
+    useEffect(() => { calProgress() }, [count, items.length])
     function search() {
         //alert(song)
-        ItemsList([...items, { key: val, value: item,checked:checked }])
-        keyVal(p => p + 1)
-        //alert(val)
-        setCurrentItem("")
+        if (item !== null && item.trim().length > 0) {
+            ItemsList([...items, { key: val, value: item, checked: checked }])
+            keyVal(p => p + 1)
+            //alert(val)
+            setCurrentItem("")
+        }
+        else {
+            //alert("Run")
+            //handleClick()
+            handleClick({ vertical: 'top', horizontal: 'center' })
+            setCurrentItem("")
+        }
+
 
     }
-    function calProgress(){
+    function calProgress() {
         //alert()
         //alert("call")
-        setProgress((count/items.length)*100)
+        setProgress((count / items.length) * 100)
     }
-    function rmItem(key,checked) {
+    function rmItem(key, checked) {
 
         ItemsList(items.filter(x => x.key !== key))
-        if(checked){
+        if (checked) {
             //alert("yes")
-            setCount(p=>p-1)
+            setCount(p => p - 1)
         }
-        
+
         //calProgress()
     }
 
-    function totalChecked(isChecked,curr){
+    function totalChecked(isChecked, curr) {
         //alert(isChecked)
         //setChecked(isChecked)
-        let idx = items.findIndex(e=>e.key==curr)
+        let idx = items.findIndex(e => e.key == curr)
         items[idx].checked = isChecked
-        if(isChecked){
-            setCount(p=>p+1)
+        if (isChecked) {
+            setCount(p => p + 1)
 
         }
-        else{
-            setCount(p=>p-1)
+        else {
+            setCount(p => p - 1)
         }
         //alert(count)
-        
-        
+
+
     }
     return <div className="container">
+
+        <Snackbar
+            anchorOrigin={{ vertical, horizontal }}
+            open={open}
+            onClose={handleClose}
+            autoHideDuration={3000}
+            message="Cannot add empty to-do"
+            key={vertical + horizontal}
+        />
 
         <div id="imgWrapper">
             <img src={logo} height="100" width="100" />
@@ -90,49 +104,80 @@ export default function Header(props) {
         </div>
 
         <div className="mainBody">
-            <div className="fGrow">
-            <ul type="none" style={{padding:"10px"}}>
+            <Card className="fGrow">
                 
-                {items.map(obj => {
-                    return (
-                        <li key={obj.key}>
-                            <div className="item">
-                                <div>
-                                    {obj.value}
-                                </div>
+                {items.length > 0 ? <ul type="none" style={{ padding: "10px" }}>
 
-                                <div>
-                                    <input type="checkbox" onChange={(e)=>totalChecked(e.target.checked,obj.key)
-                                    } />
-                                    <IconButton onClick={() => { rmItem(obj.key,obj.checked) }}>
-                                        <DeleteIcon />
-                                    </IconButton>
-                                </div>
+                    {items.map(obj => {
+                        return (
+                            <li key={obj.key}>
+                                <Card className="item">
+                                    <div>
+                                        {obj.value}
+                                    </div>
+
+                                    <div>
+                                        <input type="checkbox" onChange={(e) => totalChecked(e.target.checked, obj.key)
+                                        } />
+                                        <IconButton onClick={() => { rmItem(obj.key, obj.checked) }}>
+                                            <DeleteIcon />
+                                        </IconButton>
+                                    </div>
 
 
 
 
-                            </div>
-                            
-                        </li>
+                                </Card>
+                                <Divider />
 
-                    )
-                })}
-                
-            </ul>
-            </div>
+                            </li>
 
-            <div className="fGrow">
-            <h1>To-do progress stats</h1>
-                <div className="right">
-                    
-                    <div>
-                        <CircularProgressWithLabel value={progress} />
+                        )
+                    })}
+
+                </ul> :
+
+                    <div className="inner">
+                        <h1>To-do List</h1>
+                        <HourglassEmptyIcon style={{ fontSize: 100 }} />
+                        <p>Todo list empty &nbsp;&nbsp;</p>
+
                     </div>
-                </div>
-            </div>
 
-            
+                }
+            </Card>
+
+            <Card className="fGrow">
+                <div className="inner" style={{backgroundColor:"#fccbcb"}}>
+                    <h1>To-do progress stats</h1>
+                </div>
+
+                {items.length > 0 ?
+
+
+                    <div className="right">
+
+                        <div className="inner">
+
+                            <h3>Completion percentage</h3>
+                            <CircularProgressWithLabel value={progress} />
+                            <p>Total tasks completed:{count}/{items.length}</p>
+
+                        </div>
+                        <div className="inner">
+                            <h3>Remaining taks percentage</h3>
+                            <CircularProgressWithLabel value={100 - progress} />
+                            <p>Remaining tasks:{items.length - count}</p>
+                        </div>
+                    </div> :
+                    <div className="inner" style={{backgroundColor:"#fccbcb"}}>
+                        <DateRangeIcon style={{ fontSize: 100 }} />
+                        <p> Your progress stats will be shown here.</p>
+                    </div>}
+
+            </Card>
+
+
         </div>
 
 
---
+++ src/components/Items.js
@@ -0,0 +1,9 @@
+import React,{useState} from 'react';
+import  ReactDOM  from 'react-dom';
+
+
+export function Items(props){
+    const [item,itemlist] = useState([])
+    alert(item)
+    
+}
