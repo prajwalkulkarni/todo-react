59f907e1ab9d8d5969c1e28a38d41b33e7d87b08
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-08-29 23:16:03 +0530

    Add component wrapper

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import './Card.css'
+export default function Card(props){
+
+    const className = 'card '+props.className
+    return(
+        <div className={className}>
+            {props.children}
+
+        </div>
+
+    )
+}
